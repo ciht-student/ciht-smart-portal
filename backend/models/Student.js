d1bbf16d7b740fe10fe5da108a535d5469ac9a62
ciht-student <cihtstudent@gmail.com>
2026-04-28 06:51:50 -0700

    Student.js

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -33,7 +33,7 @@ const studentSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
-  }
+  },
    userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
